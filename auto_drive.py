fd4eca34d4c5dc9760cbb2028a31fa6b15470f73
Rodrigo <rmcantini@gmail.com>
2022-05-11 13:11:38 -0300

    tests with cleaning strings

--- auto_drive.py
+++ auto_drive.py
@@ -9,10 +9,17 @@ PATH_DESTINATION = '/Volumes/GoogleDrive-105809374960814147854/My Drive/Angeloni
 
 # get string with folder names
 TASKS_PAUTA = input('Cole as tasks da pauta aqui: ')
-LISTA_TASKS = TASKS_PAUTA.split('#')
+lista_tasks = TASKS_PAUTA.split('#')
+# print(lista_tasks)
 
-print(LISTA_TASKS)
 
 # clean folder names
 
 # loop to copy files from origin to destination joining the path + folder name
+
+
+# >>> mystr = "L1\nL2\n\nL3\nL4\n  \n\nL5"
+# >>> mystr.split('\n')
+# ['L1', 'L2', '', 'L3', 'L4', '  ', '', 'L5']
+# >>> [line for line in mystr.split('\n') if line.strip() != '']
+# ['L1', 'L2', 'L3', 'L4', 'L5']
