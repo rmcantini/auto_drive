6841a9a138c96c9f6a51dc1389a9dad4217a94a3
Rodrigo <rmcantini@gmail.com>
2022-05-12 16:01:16 -0300

    more tests no success

--- auto_drive.py
+++ auto_drive.py
@@ -2,6 +2,11 @@
 
 
 # origin path
+
+
+from csv import list_dialects
+
+
 PATH_ORIGIN = '/Users/rodrigocantini/Documents/Angeloni'
 
 # destination path
@@ -9,17 +14,4 @@ PATH_DESTINATION = '/Volumes/GoogleDrive-105809374960814147854/My Drive/Angeloni
 
 # get string with folder names
 TASKS_PAUTA = input('Cole as tasks da pauta aqui: ')
-lista_tasks = TASKS_PAUTA.split('#')
-# print(lista_tasks)
-
-
-# clean folder names
-
 # loop to copy files from origin to destination joining the path + folder name
-
-
-# >>> mystr = "L1\nL2\n\nL3\nL4\n  \n\nL5"
-# >>> mystr.split('\n')
-# ['L1', 'L2', '', 'L3', 'L4', '  ', '', 'L5']
-# >>> [line for line in mystr.split('\n') if line.strip() != '']
-# ['L1', 'L2', 'L3', 'L4', 'L5']
